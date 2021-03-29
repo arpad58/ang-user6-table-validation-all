ed3879e66e7dc55313e56a8fc24678ae5d1ce581
Arpad <solar@hu.inter.net>
2021-03-29 18:31:48 +0200

    table works

--- src/app/page/user-list/user-list.component.ts
+++ src/app/page/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,7 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  /* table */
+  userList$: Observable<User[]> = this.userService.getAll();
+
+  constructor(
+    private userService: UserService,    /* table */
+  ) { }
 
   ngOnInit(): void {
   }
